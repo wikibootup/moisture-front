b21b8c57f7d6fdeb12c90f8cfa6e59f1b6df71ff
wikibootup <wikibootup@gmail.com>
2017-12-24 23:13:03 +0900

    Add simple search request

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { moistureInput: '' };
+
+    this.handleMoistureInputChange = this.handleMoistureInputChange.bind(this);
+    this.query = this.query.bind(this);
+  }
+
+  handleMoistureInputChange(e) {
+    this.setState({moistureInput: e.target.value})
+  }
+
+  query(e) {
+    if (e.key === 'Enter') {
+      //@NOTE - It has been hardcoded for test
+      fetch('http://localhost:8080/api/search/' + this.state.moistureInput ).then(result => result)
+    }
+  }
+
   render() {
     return (
      <div>
         <div className="central-box">
-          <form>
-            <input className="moisture-input" type="text" autoFocus />
-          </form>
+          <input type="text" className="moisture-input"
+                 value={this.state.moistureInput}
+                 onChange={this.handleMoistureInputChange}
+                 onKeyPress={this.query}
+                 autoFocus />
         </div>
       </div>
     );
